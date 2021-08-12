7baacca1c3c55527d06cd4ba63e40c2b00371d66
vagrant <vagrant@homestead>
2021-08-12 10:32:52 +0000

    修改收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -8,16 +8,19 @@ use Illuminate\Http\Request;
 
 class UserAddressesController extends Controller
 {
+    // 列表
     public function index(Request $request)
     {
         return view('user_addresses.index', ['addresses' => $request->user()->addresses]);
     }
 
+    // 添加
     public function create()
     {
         return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
     }
 
+    // 创建
     public function store(UserAddressRequest $request)
     {
         $request->user()->addresses()->create($request->only([
@@ -31,4 +34,28 @@ class UserAddressesController extends Controller
         ]));
         return redirect()->route('user_addresses.index');
     }
+
+    // 编辑
+    public function edit(UserAddress $user_address)
+    {
+        return view('user_addresses.create_and_edit', ['address' => $user_address]);
+    }
+
+    // 修改
+    public function update(UserAddress $user_address, UserAddressRequest $request)
+    {
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+        return redirect()->route('user_addresses.index');
+    }
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,12 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
 
     Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
-
+    // 添加
     Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+
+    Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
+    // 修改
+    Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
 
 });
 
